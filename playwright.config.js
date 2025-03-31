9a3d3a3bcb0680b4743f85eb4fc26806199f206d
touil-mohamed <touil.mohamed1@hotmail.com>
2025-03-31 17:22:49 +0200

    perf: :zap: add webServer config

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './src/tests/e2e',
-  timeout: 30000,
-  expect: {
-    timeout: 5000
-  },
+  testDir: './src/test/E2E',
   use: {
     baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
+  },
+  webServer: {
+    command: 'npm run dev',
+    port: 5173,
+    reuseExistingServer: true,
+    timeout: 120000,
   },
 });
